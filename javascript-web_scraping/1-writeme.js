ae04186b2e0a691a0ebfa5628a9d0ff0356828b0
fchavonet <fchavonet@mailo.com>
2024-01-22 10:35:29 +0100

    Firest upload of task "1. Write me".

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,11 +2,10 @@
 
 const fs = require('fs');
 const filePath = process.argv[2];
+const content = process.argv[3];
 
-fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
     console.error(err);
-  } else {
-    console.log(data);
   }
 });
