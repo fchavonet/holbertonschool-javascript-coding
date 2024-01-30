8ca1596e0021f1e7c8ffbd971969becba58fc3c2
fchavonet <fchavonet@mailo.com>
2024-01-30 13:01:44 +0100

    Add detailed comments (for the learning experience).

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,11 +1,15 @@
 const http = require('http');
 
+// Create an HTTP server.
 const app = http.createServer((req, res) => {
+  // Set the response HTTP header with status code 200 and Content-Type text/plain.
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
+  // Send the response body with "Hello Holberton School!".
   res.end('Hello Holberton School!\n');
 });
 
+// Listen on port 1245
 app.listen(1245, () => {
   console.log('Server running at http://localhost:1245/');
 });
