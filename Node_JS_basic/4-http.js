c3174fc58badefeded2d8b435e781769cc4ff07c
fchavonet <fchavonet@mailo.com>
2024-02-01 08:59:37 +0100

    Fix: remove `/n`.

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -6,7 +6,7 @@ const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
   // Send the response body with "Hello Holberton School!".
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 
 // Listen on port 1245
